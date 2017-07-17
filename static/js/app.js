e1aa6a2af2eb7478fd43b3155fcbc19c86da1457
Aman Bhayani <amanbhayani@hotmail.com>
2017-07-17 00:20:12 -0700

    working on heroku now?

---
+++ static/js/app.js
@@ -0,0 +1,37 @@
+
+
+var socket = io.connect('http://localhost:5000');
+socket.on('connect', function(){
+	socket.emit('event', {
+		data: 'User Connected'
+	});
+});
+
+	//capture message 
+socket.on ('my response', function(msg){
+	console.log(msg);
+})
+
+
+var form = $( 'form').on('submit', function ( event ){
+	// to allow for when the user hits enter
+	event.preventDefault();
+	var user_name = $('input.username').val();
+	var message   = $('input.message').val();
+
+	socket.emit( 'event', {
+		user : user_name,
+		msg : message
+	});
+
+	console.log(user_name, message);
+
+});
+
+// Capture Message
+socket.on('my response', function( msg ){
+	if( typeof msg.user !== 'undefined' ){
+		$('h1').remove();
+		$('div.message_holder').append('<div class="message_roll"><b>' + msg.user + ': </b>' + msg.msg + '</div>' );
+	}
+})
