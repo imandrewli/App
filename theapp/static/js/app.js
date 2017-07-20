5b040c64d38780c7593b0daa40d1c3c6f7a3898d
Andrew Li <androo@Andrews-MacBook-Pro.local>
2017-07-20 15:29:44 -0700

    clear text box on form submit

--- theapp/static/js/app.js
+++ theapp/static/js/app.js
@@ -35,6 +35,7 @@ socket.on('my response', function( msg ){
 		$('h1').remove();
 		$('div.message_holder').append('<div class="message_roll"><b>' + msg.user + ': </b>' + msg.msg + '</div>' );
 		$('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
+		$('#chatinputbox').val('');
 	}
 })
 
