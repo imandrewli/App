6fb0a5b5240ab2dc5f447e90e11555f8928285c3
Andrew Li <androo@Andrews-MacBook-Pro.local>
2017-07-20 17:06:30 -0700

    comment

--- theapp/static/js/app.js
+++ theapp/static/js/app.js
@@ -37,6 +37,7 @@ socket.on('my response', function( msg ){
 		$('h1').remove();
 		$('div.message_holder').append('<div class="message_roll"><b>' + msg.user + ': </b>' + msg.msg + '</div>' );
 		$('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
+		// clear textbox
 		$('#chatinputbox').val('');
 	}
 })
