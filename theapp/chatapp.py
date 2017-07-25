d12d4c7c1a709a0664354402fbd16a5463449540
SethTolkamp <stolkamp@uvic.ca>
2017-07-25 08:28:24 -0700

    added the \history command

--- theapp/chatapp.py
+++ theapp/chatapp.py
@@ -5,6 +5,10 @@ from flask import session
 from flask_socketio import emit
 from flask_socketio import join_room, leave_room
 
+room_list = []
+history = []
+room_dict = {}
+
 @myapp.route('/')
 def index():
     return render_template('splash.html')
@@ -20,13 +24,38 @@ def get_file_name():
 @socketio.on('message')
 def on_message(json):
     print('received something chat:' + str(json))
-    room = json['room']
-    print(room)
-    emit('message response', json, room=room)
+    room = json['room']        
+
+    if str(json['msg']) == '\history':
+        emit('history req', json, room = room)
+        for item in room_dict[str(room)]:
+            emit('message response', item, room=room)
+
+    else:      
+        room_dict[str(room)].append(json)
+        emit('message response', json, room=room)
+
 
 @socketio.on('join')
 def on_join(json):
     alias = json['alias']
     room = json['room']
     join_room(room)
+
+    print('joined room: ' + room)
+    
+    if room in room_dict:
+        x+1
+    else:
+        room_dict[str(room)] = []
+        print('\n room dict: ')
+        print( room_dict)
+
+    
     emit('join response', json, room=room)
+
+    emit('history req', json, room = room) # I cant get this line to display anything in the chat room.
+    # for item in room_dict[str(room)]:
+    #     emit('message response', item, room=room)
+           
+    
