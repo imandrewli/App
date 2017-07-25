bc0404143726046c087c28d5ba8482268bb3e1a3
Andrew Li <androo@Andrews-MacBook-Pro.local>
2017-07-24 19:13:03 -0700

    moved chat room label

--- theapp/static/js/chat.js
+++ theapp/static/js/chat.js
@@ -7,6 +7,7 @@ socket.on('connect', function(){
         alias: alias,
         room:  room
     });
+    document.getElementById("chat_room_name").innerHTML=room;
 });
 
 //Capture users joining
@@ -97,3 +98,4 @@ $('#join_room').on('click', function(e){
         window.location = linkLocation;
     }  
 });
+
