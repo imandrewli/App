a4da17e950455eae78f641fb904454823c0e2e6d
SethTolkamp <stolkamp@uvic.ca>
2017-08-01 15:47:16 -0700

    25 past msges now sent on join

--- theapp/chatapp.py
+++ theapp/chatapp.py
@@ -5,8 +5,6 @@ from flask import session
 from flask_socketio import emit
 from flask_socketio import join_room, leave_room
 
-room_list = []
-history = []
 room_dict = {}
 
 @myapp.route('/')
@@ -23,12 +21,11 @@ def get_file_name():
 
 @socketio.on('message')
 def on_message(json):
-    print('received something chat:' + str(json))
-    room = json['room']        
-  
-    room_dict[str(room)].append(json)
-    emit('message response', json, room=room)
-
+	room = json['room']  
+	room_dict[str(room)].append(json)
+	emit('message response', json, room=room)
+	if ( len(room_dict[str(room)]) > 25 ):
+		room_dict[str(room)].pop(0)
 
 @socketio.on('join')
 def on_join(json):
@@ -36,12 +33,13 @@ def on_join(json):
     room = json['room']
     join_room(room)
 
-    print('joined room: ' + room)
-    
-    if room not in room_dict:
-        room_dict[str(room)] = []
-    
     emit('join response', json, room=room)
 
-    for item in room_dict[str(room)]:
-        emit('message response', item)
+    if room not in room_dict:
+        room_dict[str(room)] = []
+        emit('new room msg', json)
+    else:
+    	emit('history req', json)
+    	for item in room_dict[str(room)]:
+    		emit('message response', item)
+
